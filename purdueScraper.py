3eecc1d1f3823b6f7dab5c7f178a42d34f67ae3b
Drew Ruberson <aruberso@purdue.edu>
2013-04-01 10:59:20 -0400

    Working on getting hidden header elements
    Self explanatory

--- purdueScraper.py
+++ purdueScraper.py
@@ -1,24 +1,29 @@
 import requests
+from bs4 import BeautifulSoup
 
 loginUrl = 'https://homebanking.purduefed.com/OnlineBanking/Login.aspx'
 passwordUrl = 'https://homebanking.purduefed.com/OnlineBanking/AOP/Password.aspx'
 accountUrl = 'https://homebanking.purduefed.com/OnlineBanking/AccountSummary.aspx'
-
-testUrl = 'http://web.ics.purdue.edu/~powerst/systfp/login.php'
-destUrl = 'http://web.ics.purdue.edu/~powerst/systfp/home.php'
-
-tN = 'Shawn'
-tP = 'abc'
-
-val = {'username' : tN, 'password' : tP}
 
 loginName = 'abravoset'
 password = 'CS307project'
 
 values = {'LoginName' : loginName, 'Password' : password}
 headers = {'User-agent' : 'Mozilla/5.0 (Windows; U; Windows NT 5.1; de; rv:1.9.1.5) Gecko/20091102 Firefox/3.5.5'}
-r = requests.post(testUrl, data=val, headers=headers)
-#r2 = requests.post(accountUrl, auth=HTTPDigestAuth(loginName, password), headers=headers)
 
-print r.url
+r = requests.get(loginUrl, headers=headers)
 
+# Stores HTML of r to soup
+soup = BeautifulSoup(r.text)
+
+l = list()
+
+
+# Set to loop through soup to find necessary input params
+for item in soup.find_all('input'):
+	if 'name' in item.attrs:
+		if "LoginName" in item.attrs['name'] :
+			item.attrs['value'] = loginName
+		l.append({item.attrs['name'] : (item.attrs['value'] if 'value' in item.attrs else '')})	
+
+r2 = requests.post(loginUrl, params=l, headers=headers)

7285b80729d32838094ffcc6d498ef313e68ac71
Drew Ruberson <aruberso@purdue.edu>
2013-03-31 22:06:12 -0400

    Testing out a new type of scraper
    Utilizes the python requests library

---
+++ purdueScraper.py
@@ -0,0 +1,14 @@
+import requests
+
+loginUrl = 'https://homebanking.purduefed.com/OnlineBanking/Login.aspx'
+passwordUrl = 'https://homebanking.purduefed.com/OnlineBanking/AOP/Password.aspx'
+accountUrl = 'https://homebanking.purduefed.com/OnlineBanking/AccountSummary.aspx'
+
+loginName = 'abravoset'
+password = 'CS307student'
+
+values = {'LoginName': loginName, 'Password':password}
+headers = {'User-agent': 'Mozilla/5.0 (Windows; U; Windows NT 5.1; de; rv:1.9.1.5) Gecko/20091102 Firefox/3.5.5'}
+r = requests.post(loginUrl, data=values, headers=headers)
+r2 = requests.post(passwordUrl, data=values, headers=headers)
+

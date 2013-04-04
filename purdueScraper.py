ee1d4c624e826399bb12f1e9b1f0ff1ada1bd1d4
Shawn Carmichael <scarmich@purdue.edu>
2013-04-04 16:23:41 -0400

    Changed scraper

--- purdueScraper.py
+++ purdueScraper.py
@@ -2,13 +2,14 @@ import requests
 
 loginUrl = 'https://homebanking.purduefed.com/OnlineBanking/Login.aspx'
 passwordUrl = 'https://homebanking.purduefed.com/OnlineBanking/AOP/Password.aspx'
-accountUrl = 'https://homebanking.purduefed.com/OnlineBanking/AccountSummary.aspx'
+acctUrl = 'https://homebanking.purduefed.com/OnlineBanking/AccountSummary.aspx'
 
 loginName = 'abravoset'
-password = 'CS307student'
+password = 'CS307project'
 
-values = {'LoginName': loginName, 'Password':password}
+values = {'LoginName' : loginName, 'Password' : password}
 headers = {'User-agent': 'Mozilla/5.0 (Windows; U; Windows NT 5.1; de; rv:1.9.1.5) Gecko/20091102 Firefox/3.5.5'}
+
 r = requests.post(loginUrl, data=values, headers=headers)
 r2 = requests.post(passwordUrl, data=values, headers=headers)
-
+print r.url

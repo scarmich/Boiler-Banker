febcf9541f9cbab331851891314b7d2be20a7097
Drew Ruberson <aruberso@purdue.edu>
2013-04-04 22:39:48 -0400

    Offline view is properly set
    If the offline view checkbox isn't set in the settings menu, the user
    is unable to access the offline view.

--- BoilerBankerDemo/src/com/example/boilerbanker/MainActivity.java
+++ BoilerBankerDemo/src/com/example/boilerbanker/MainActivity.java
@@ -1,25 +1,47 @@
 package com.example.boilerbanker;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.Menu;
 import android.view.View;
 import android.widget.EditText;
-import android.widget.TextView;
 
 public class MainActivity extends Activity {
 	
 	public void openOfflineView(View view) {
-		//Takes the user to the offline view
-		Intent offlineIntent = new Intent(this, DisplayOfflineViewActivity.class);
-		startActivity(offlineIntent);
+		// Checks to see if OfflineView is enabled
+		SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(this);
+		boolean checked = sp.getBoolean("OFFLINE_CHECKBOX", false);
+		
+		/*
+		 * If checked is true, begins the new intent and opens the offline
+		 * view. If false, informs the user
+		 */
+		if (checked) {
+			Intent offlineIntent = new Intent(this, DisplayOfflineViewActivity.class);
+			startActivity(offlineIntent);
+		} else {
+			AlertDialog.Builder alertDialog = new AlertDialog.Builder(this);
+			alertDialog.setTitle("UH OH!");
+			alertDialog.setMessage("Offline View is not set!");
+			alertDialog.setNeutralButton("OK", new DialogInterface.OnClickListener() {
+				public void onClick(DialogInterface dialog, int which) {
+					return;
+				}
+			});
+			alertDialog.show();
+		}
 	}
 	
 	public void openWelcome(View view) {
 		
 		/*
-		 * Grabs username and password credentials and saves them to login 
+		 * Grabs USERNAME and password credentials and saves them to login 
 		 * and password variables
 		 */
 		EditText usernameEditText = (EditText) this.findViewById(R.id.username_message);

42686059858441d71518271c00b58b014f3f624d
Shawn Carmichael <scarmich@purdue.edu>
2013-04-11 16:13:14 -0400

    Added HTML Parser to Server Folder

---
+++ BoilerServer/src/webParser.py
@@ -0,0 +1,12 @@
+from HTMLParser import HTMLParser
+
+class MyHTMLParser(HTMLParser):
+   def handle_starttag(self, tag, attrs):
+      print "Encountered a start tag:", tag
+   def handle_endtag(self, tag):
+      print "Encountered an end tag :", tag
+   def handle_data(self, data):
+      print "Encountered some data  :", data
+
+parser = MyHTMLParser()
+parser.feed('<html><body><input>Transaction Data</input></body></html>')

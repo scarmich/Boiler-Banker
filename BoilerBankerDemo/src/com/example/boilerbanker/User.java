98c9d8ee5ac4a64933ab91f1770eddc622b3ec84
Adam Rea <area@purdue.edu>
2013-04-16 18:00:07 -0400

    Revert "Changed Transaction and User to support parsing.  Tried to also do some things with gitignore.."
    This reverts commit 170815aeb099dbcaf3c4cff03736d4b30eea9cdb.

--- BoilerBankerDemo/src/com/example/boilerbanker/User.java
+++ BoilerBankerDemo/src/com/example/boilerbanker/User.java
@@ -9,52 +9,38 @@ public class User implements Serializable{
 	private static final long serialVersionUID = 1L;
 	private String userName;
 	private Transaction[] transactions;
-	private double balance;
 	private int numTransactions;
+	private double balance;
 	
 	
 	public User(String userName){
 		this.userName = userName;
 		transactions = new Transaction[5];
-		String fileName = "UserTransactions/" + userName + ".txt";
+		String fileName = userName + ".txt";
 		File f = new File(fileName);
 		
 		double amount;
 		String location;
 		String date;
-		double bal;
 		
 		try {
 			Scanner in = new Scanner(f);
-			//throw away first two lines of file (aka descriptor lines)
-			in.nextLine();
-			in.nextLine();
-			//balance = Double.parseDouble(in.nextLine());
-			//numTransactions = Integer.parseInt(in.nextLine());
-			int i;
-			for(i = 0; in.hasNextLine(); i++){
+			balance = Double.parseDouble(in.nextLine());
+			numTransactions = Integer.parseInt(in.nextLine());
+			for(int i = 0; i < numTransactions; i++){
 				transactions[i] = new Transaction();
+				amount = in.nextDouble();
 				date = in.next();
 				location = in.next();
-				amount = Double.parseDouble(in.next());
-				bal = Double.parseDouble(in.next());
 				
 				transactions[i].setId(i);
 				transactions[i].setAmount(amount);
 				transactions[i].setLocation(location);
 				transactions[i].setDate(date);
-				transactions[i].setBalance(bal);
 			}
-			numTransactions = i;
-			
-			for(;i < 50; i++) {
-				transactions[i] = new Transaction();
-			}
-			balance = transactions[0].getBalance();
 			in.close();
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
-			System.exit(-1);
 		}
 		
 		
@@ -72,7 +58,7 @@ public class User implements Serializable{
 		return transactions;
 	}
 	
-	public int getNumTransactions() {
+	public int getNumTransactions(){
 		return numTransactions;
 	}
 	

606d29f6f2b97965e2b8a34e9da5c271f8963b89
Austin Miller <admiller30@gmail.com>
2013-04-17 15:23:41 -0400

    Added Category class
    I wish gitignore worked... I keep overlooking files in this giant list
    of crap that I don't want to commit

---
+++ BoilerBankerDemo/src/com/example/boilerbanker/Category.java
@@ -0,0 +1,31 @@
+package com.example.boilerbanker;
+
+import java.util.ArrayList;
+
+public class Category {
+	
+	private ArrayList<Transaction> transactions;
+	private String name;
+	
+	public Category(String name) {
+		this.name = name;
+		transactions = new ArrayList<Transaction>();
+	}
+	
+	public void addTransaction(Transaction trans) {
+		transactions.add(trans);
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public double getAmount() {
+		double amount = 0;
+		for(int i = 0; i < transactions.size(); i++) {
+			amount += transactions.get(i).getAmount();
+		}
+		return amount;
+	}
+
+}
